f0e2c1d14252f073bd7519edc453e07c7206f2ad
Wojtek Dasiukiewicz <wojtek@octocode.pl>
2020-06-20 11:57:35 +0200

    Music issue fix

--- app/js/EnterScreen.js
+++ app/js/EnterScreen.js
@@ -1,6 +1,6 @@
 import React, {useState,useEffect} from 'react';
 
-const EnterScreen = ({changeScreen}) =>{
+const EnterScreen = ({setGameStatusHandler}) =>{
 
     const [startOpacity,setStartOpacity]=useState(60);
     const [increase,setIncrease]=useState(true);
@@ -41,7 +41,7 @@ const EnterScreen = ({changeScreen}) =>{
         <div className='enterScreen'>
             <h1>Western Shot</h1>
             <p >You are the last man stand who can free a city from evil bastards. You must kill 50 opponents to reach the goal. Be careful, the city is so big that you can't see all the buildings. You have to look around. Bandits can hide everywhere. Every time they want to shoot you, they must reload their weapons and have some time to aim at you. So... when you hear that, you can be sure that a bastard has appeared for shoot down.</p>
-            <h2 onClick={changeScreen} style={{opacity:startOpacity+'%'}}>Start a game</h2>
+            <h2 onClick={() => setGameStatusHandler(1)} style={{opacity:startOpacity+'%'}}>Start a game</h2>
         </div>
     );
 };
--- app/js/Game.js
+++ app/js/Game.js
@@ -5,7 +5,7 @@ import GameOver from '../js/GameOver';
 
 class Game extends Component {
     state= {
-        life: 3,
+        life: 1,
         kills:0,
         bandits:[{left:18.2+'vw',top:56+'vh', visible:false},{left:30.7+'vw',top:56+'vh', visible:false},{left:52.8+'vw',top:60+'vh', visible:false},{left:68+'vw',top:56+'vh', visible:false},{left:76.5+'vw',top:58+'vh', visible:false},{left:83.5+'vw',top:56+'vh', visible:false},{left:90.3+'vw',top:58+'vh', visible:false},{left:105.3+'vw',top:57+'vh', visible:false},{left:111.8+'vw',top:57+'vh', visible:false},{left:136.2+'vw',top:57+'vh', visible:false},{left:142.6+'vw',top:57+'vh', visible:false},{left:164.3+'vw',top:57+'vh', visible:false},{left:176.6+'vw',top:57+'vh', visible:false},{left:187.8+'vw',top:57+'vh', visible:false},{left:201.2+'vw',top:57+'vh', visible:false},{left:215+'vw',top:57+'vh', visible:false},{left:234.4+'vw',top:57+'vh', visible:false},{left:242.3+'vw',top:57+'vh', visible:false},{left:248.8+'vw',top:55+'vh', visible:false},{left:255+'vw',top:55+'vh', visible:false},{left:261.3+'vw',top:55+'vh', visible:false},{left:16+'vw',top:34+'vh', visible:false},{left:24.7+'vw',top:22+'vh', visible:false},{left:34+'vw',top:33+'vh', visible:false},{left:44+'vw',top:35+'vh', visible:false},{left:49.8+'vw',top:35+'vh', visible:false},{left:55.6+'vw',top:35+'vh', visible:false},{left:42+'vw',top:22+'vh', visible:false},{left:50+'vw',top:14+'vh', visible:false},{left:58+'vw',top:22+'vh', visible:false},{left:75.6+'vw',top:35+'vh', visible:false},{left:80.7+'vw',top:35+'vh', visible:false},{left:85.8+'vw',top:35+'vh', visible:false},{left:91+'vw',top:35+'vh', visible:false},{left:75+'vw',top:10 +'vh', visible:false},{left:83.2+'vw',top:1+'vh', visible:false},{left:92+'vw',top:10+'vh', visible:false},{left:104.2+'vw',top:35+'vh', visible:false},{left:111+'vw',top:35+'vh', visible:false},{left:117.5+'vw',top:35+'vh', visible:false},{left:124.1+'vw',top:35+'vh', visible:false},{left:130.7+'vw',top:35+'vh', visible:false},{left:137.3+'vw',top:35+'vh', visible:false},{left:144+'vw',top:35+'vh', visible:false},{left:107+'vw',top:0.8+'vh', visible:false},{left:142+'vw',top:0.8+'vh', visible:false},{left:164+'vw',top:33+'vh', visible:false},{left:170.5+'vw',top:33+'vh', visible:false},{left:176.7+'vw',top:33+'vh', visible:false},{left:162+'vw',top:12+'vh', visible:false},{left:170.5+'vw',top:4+'vh', visible:false},{left:179+'vw',top:12+'vh', visible:false},{left:187.5+'vw',top:33+'vh', visible:false},{left:194.5+'vw',top:33+'vh', visible:false},{left:201.3+'vw',top:33+'vh', visible:false},{left:185+'vw',top:15+'vh', visible:false},{left:194.5+'vw',top:4+'vh', visible:false},{left:204+'vw',top:15+'vh', visible:false},{left:224.4+'vw',top:6+'vh', visible:false},{left:214+'vw',top:31+'vh', visible:false},{left:235+'vw',top:31+'vh', visible:false},{left:246+'vw',top:30+'vh', visible:false},{left:255+'vw',top:24+'vh', visible:false},{left:263.5+'vw',top:30+'vh', visible:false}]
     } 
@@ -25,7 +25,6 @@ class Game extends Component {
     };
 
     componentDidMount() {
-        this.props.musicOff()
         this.mainInterval=setInterval( () => {
             this.showBandit();
             const creakWindow = new Audio('../../assets/sounds/bandit.shot/reload.mp3');
@@ -48,6 +47,15 @@ class Game extends Component {
         clearTimeout(this.deathInterval);
     };
 
+    componentDidUpdate() {
+        if(this.state.kills==50 && this.state.life>0){
+            // this.props.setGameStatusHandler(2);
+        }
+        if(this.state.life === 0 ){
+            this.props.setGameStatusHandler(2);
+        }
+    }
+
     handleClick = banditIndex => {
         clearTimeout(this.deathInterval);
         this.setState(prevState => {
@@ -62,18 +70,8 @@ class Game extends Component {
         })
     };
 
-    render() {
-        if (this.state.life>0 && this.state.kills<50){
-            return <GameScreen bandits={this.state.bandits} life={this.state.life} kills={this.state.kills} handleClick={this.handleClick}/>
-        } else if(this.state.kills==50 && this.state.life>0){
-            clearInterval(this.mainInterval);
-            clearTimeout(this.deathInterval);
-            return <Win/>
-        } else {
-            clearInterval(this.mainInterval);
-            clearTimeout(this.deathInterval);
-            return <GameOver killsScore={this.state.kills}/>
-        }
+    render() {        
+        return <GameScreen bandits={this.state.bandits} life={this.state.life} kills={this.state.kills} handleClick={this.handleClick}/>        
     }
 }
 
