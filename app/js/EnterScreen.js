7c4bb04dd6b5777817702ddb9e131009283a40f6
Wojciech Kiryk <wojciech.kiryk@gmail.com>
2020-06-27 17:54:36 +0200

    little corrects

--- app/js/EnterScreen.js
+++ app/js/EnterScreen.js
@@ -6,7 +6,7 @@ const EnterScreen = ({setGameStatusHandler}) =>{
     return (
         <div className='enterScreen'>
             <h1>Western Shot</h1>
-            <p >You are the last man stand who can free a city from evil bastards. You must kill 50 opponents to reach the goal. Be careful, the city is so big that you can't see all the buildings. You have to look around. Bandits can hide everywhere. Every time they want to shoot you, they must reload their weapons and have some time to aim at you. So... when you hear that, you can be sure that a bastard has appeared for shoot down.</p>
+            <p > You are the last man stand who can free a city from evil bastards. You must kill 50 opponents to reach the goal. Be careful, the city is so big that you can't see all the buildings. You have to look around. Bandits can hide everywhere. Every time they want to shoot you, they must reload their weapons and have some time to aim at you. So... when you hear that, you can be sure that a bastard has appeared for shoot down. It is speedy observation game so you should be focus. Good luck!</p>
             <h2 onClick={() => setGameStatusHandler(GameStatus.GAME)} className='blink_me'>Start a game</h2>
         </div>
     );
--- app/js/Game.js
+++ app/js/Game.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import GameScreen from './GameScreen';
-import Bandits from './data/bandits';
+//import Bandits from './data/bandits'; // imported Bandits doesn't reset state 'visible' on start again. 
 import GameStatus from './constants/GameStatus';
 
 class Game extends Component {
     state= {
         life: 3,
         kills:0,
-        bandits:Bandits
+        bandits:[{left:'18.2vw',top:'56vh', visible:false},{left:'30.7vw',top:'56vh', visible:false},{left:'52.8vw',top:'60vh', visible:false},{left:'68vw',top:'56vh', visible:false},{left:'76.5vw',top:'58vh', visible:false},{left:'83.5vw',top:'56vh', visible:false},{left:'90.3vw',top:'58vh', visible:false},{left:'105.3vw',top:'57vh', visible:false},{left:'111.8vw',top:'57vh', visible:false},{left:'136.2vw',top:'57vh', visible:false},{left:'142.6vw',top:'57vh', visible:false},{left:'164.3vw',top:'57vh', visible:false},{left:'176.6vw',top:'57vh', visible:false},{left:'187.8vw',top:'57vh', visible:false},{left:'201.2vw',top:'57vh', visible:false},{left:'215vw',top:'57vh', visible:false},{left:'234.4vw',top:'57vh', visible:false},{left:'242.3vw',top:'57vh', visible:false},{left:'248.8vw',top:'55vh', visible:false},{left:'255vw',top:'55vh', visible:false},{left:'261.3vw',top:'55vh', visible:false},{left:'16vw',top:'34vh', visible:false},{left:'24.7vw',top:'22vh', visible:false},{left:'34vw',top:'33vh', visible:false},{left:'44vw',top:'35vh', visible:false},{left:'49.8vw',top:'35vh', visible:false},{left:'55.6vw',top:'35vh', visible:false},{left:'42vw',top:'22vh', visible:false},{left:'50vw',top:'14vh', visible:false},{left:'58vw',top:'22vh', visible:false},{left:'75.6vw',top:'35vh', visible:false},{left:'80.7vw',top:'35vh', visible:false},{left:'85.8vw',top:'35vh', visible:false},{left:'91vw',top:'35vh', visible:false},{left:'75vw',top:'10vh', visible:false},{left:'83.2vw',top:'1vh', visible:false},{left:'92vw',top:'10vh', visible:false},{left:'104.2vw',top:'35vh', visible:false},{left:'111vw',top:'35vh', visible:false},{left:'117.5vw',top:'35vh', visible:false},{left:'124.1vw',top:'35vh', visible:false},{left:'130.7vw',top:'35vh', visible:false},{left:'137.3vw',top:'35vh', visible:false},{left:'144vw',top:'35vh', visible:false},{left:'107vw',top:'0.8vh', visible:false},{left:'142vw',top:'0.8vh', visible:false},{left:'164vw',top:'33vh', visible:false},{left:'170.5vw',top:'33vh', visible:false},{left:'176.7vw',top:'33vh', visible:false},{left:'162vw',top:'12vh', visible:false},{left:'170.5vw',top:'4vh', visible:false},{left:'179vw',top:'12vh', visible:false},{left:'187.5vw',top:'33vh', visible:false},{left:'194.5vw',top:'33vh', visible:false},{left:'201.3vw',top:'33vh', visible:false},{left:'185vw',top:'15vh', visible:false},{left:'194.5vw',top:'4vh', visible:false},{left:'204vw',top:'15vh', visible:false},{left:'224.4vw',top:'6vh', visible:false},{left:'214vw',top:'31vh', visible:false},{left:'235vw',top:'31vh', visible:false},{left:'246vw',top:'30vh', visible:false},{left:'255vw',top:'24vh', visible:false},{left:'263.5vw',top:'30vh', visible:false}]
     } 
 
     // //This function change boolean 'visible' property on a random bandit:
